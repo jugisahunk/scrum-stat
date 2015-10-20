80693a60d4daf39177eca31c5b5b9de4c9bf8a57
Jason Knight <jugisahunk@gmail.com>
2015-10-20 14:30:40 -0500

    seperated task for watching specs from running tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
-var testWatcher = gulp.watch('spec/**/*.js', ['default']);
-
-testWatcher.on('change', function(event) {
-  console.log('Test ' + event.path + ' was ' + event.type + ', running tasks...');
-});
 
 gulp.task('default', function(){
+  console.log("These aren't the droids we're looking for. Move along...");
+});
+
+gulp.task('watch', function(){
+  gulp.watch('spec/**/*.js', ['default']);
+});
+
+gulp.task('test', function(){
   return gulp.src('spec/*.js')
     .pipe(jasmine({verbose:true, includeStackTrace: true}));
 });
