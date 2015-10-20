5fef3e651333f4cb39972da9d18b57f8d9b43f0a
Jason Knight <jugisahunk@gmail.com>
2015-10-20 15:39:44 -0500

    instructed travis to run just teh 'gulp test' task for 'npm test'

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 
-gulp.task('default', function(){
-  console.log("These aren't the droids we're looking for. Move along...");
-});
-
 gulp.task('watch', function(){
-  gulp.watch('spec/**/*.js', ['default']);
+  gulp.watch(['spec/**/*.js', 'js/**/*.js'], ['test']);
 });
 
 gulp.task('test', function(){
   return gulp.src('spec/*.js')
     .pipe(jasmine({verbose:true, includeStackTrace: true}));
 });
+
+gulp.task('default', ['test']);
